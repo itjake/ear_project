03d500283890f9e4b33750cb8e0e2f985b130d53
itjake <tokonov.tagai@gmail.com>
2022-06-19 19:36:26 +0600

    feat: added burger menu

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function () {
+  const header = document.querySelector('.header__menu');
+  window.onscroll = () => {
+    if (window.pageYOffset > 50) {
+      header.classList.add('header__active');
+    } else {
+      header.classList.remove('header__active');
+    }
+  }
+}());
+
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__menu');
+  const menuCloseItem = document.querySelector('.header__nav__close');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header__menu__active');
+  });
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header__menu__active');
+  });
+}());
+
+(function () {
+  const getElem = document.querySelector('.header__link');
+  const getElem2 = document.querySelector('.link');
+  getElem.addEventListener('click', () => {
+    getElem2.classList.add("linkoln");
+  })
+}());
